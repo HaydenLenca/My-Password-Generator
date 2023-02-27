b35d6e92b9668dd59b51df411ba4cafdfef8f9cc
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-02-27 11:08:21 -0700

    modifying variables to clean code

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,58 @@
-// Assignment code here
+var passwordLength = 8;
+var choice = [];
 
-// add code
+var specialChar = [' ','!','"','#','$','%','&','(',')','*','+',',','-','.',':',';','<','?','>','=','@','[',']','_',];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','t','z',];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var number = ['1','2','3','4','5','6','7','8','9','0',];
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var correctPrompts = getPrompts();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    if(correctPrompts) {
+        var newPassword = generatePassword();
+        passwordText.value = newPassword;
+    } else {
+        passwordText.value = "";
+    }
+  }
 
+function generatePassword() {
+
+    var password = "";
+    for(var i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * choice.length)
+        password = password + choice[randomIndex];
+    }
+    return password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getPrompts(){
+    choice = [];
+
+    passwordLength = parseInt(prompt("How many chacacters do yo want? (8 - 128) "));
+
+    if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        alert("Character lenght has to be a number between 8 and 128");
+        return false;
+    }
+
+    if (confirm("Would you like lowercase letters?")) {
+    choice = choice.concat(lowerCase);
+    }
+    if (confirm("Would you like uppedrcase letters?")) {
+        choice = choice.concat(upperCase);
+    }
+    if (confirm("Would you like special chacacters?")) {
+        choice = choice.concat(specialChar);
+    }
+    if (confirm("Would you like numbers?")) {
+        choice = choice.concat(number);
+    }
+    return true;
+
+}
